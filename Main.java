25a6179f54b5ef8dcd2f3b43b615c7168e17f35b
Shaka <ilia060864@gmail.com>
2019-03-15 20:46:36 +0500

    commit

--- Main.java
+++ Main.java
@@ -4,33 +4,5 @@
 public class Main {
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame("Компы");
-        /*Computer computer = new Computer();
-        computer.setFrequency(2.4);
-        computer.setCores(8);
-        computer.setRAM(8000);
-        computer.setMemory(1000);
-        computer.setHoldMemory(900);
-
-        Computer computer2 = new Computer();
-        computer2.setFrequency(2.4);
-        computer2.setCores(4);
-        computer2.setRAM(8000);
-        computer2.setMemory(1000);
-        computer2.setHoldMemory(900);
-        System.out.println(computer.freeMemory());
-        System.out.println(computer.copy(200));
-        System.out.println(computer.copy(99));
-        System.out.println(computer.info());
-        System.out.println(computer.compateTo(computer2));
-        System.out.println(computer.getAllMemory());
-        computer.changeMemory(2000, computer.getHoldMemory());
-        computer2.changeMemory(3000, computer2.getHoldMemory());
-        System.out.println(computer.getAllMemory());
-
-        ListNode<Computer> list = new ListNode<>();
-        list.addEnd(computer);
-        list.addEnd(computer2);
-        System.out.println(list.first.data.info());
-        System.out.println(list.last.data.info());*/
     }
 }
